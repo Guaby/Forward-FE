5516aa53dac7e9dc5b6f52981510206b579a0543
Gabriel Rosario <guaby18@gmail.com>
2024-03-05 17:12:55 -0400

    Slider hover state fix
    Fixing GSAP bugs animation for hover states.

--- pages/components/Button.tsx
+++ pages/components/Button.tsx
@@ -1,24 +1,37 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 
 const Button = () => {
 
   useEffect(() => {
     const arrowBTN = document.querySelectorAll<HTMLAnchorElement>('.arrow-btn');
-    const arrowLine = document.querySelectorAll<SVGPathElement>('.arrow-line');
 
     arrowBTN.forEach(arrowBTNElement => {
       arrowBTNElement.addEventListener('mouseover', () => {
+          const arrowLine = arrowBTNElement.querySelector<SVGPathElement>('.arrow-line');
           if (arrowLine) {
-              gsap.to(arrowLine, { attr: { d: 'M63 6H0' }, duration: 1, ease: 'pawer2.in' });
+              gsap.killTweensOf(arrowLine);
+              gsap.to(arrowLine, { 
+                  attr: { d: 'M63 6H0' }, 
+                  duration: 1, 
+                  ease: 'pawer2.in', 
+              });
           }
       });
   });
-
   arrowBTN.forEach(arrowBTNElement => {
     arrowBTNElement.addEventListener('mouseout', () => {
+        const arrowLine = arrowBTNElement.querySelector<SVGPathElement>('.arrow-line');
         if (arrowLine) {
-          gsap.to(arrowLine, { attr: { d: 'M45 6H0' }, duration: 1, ease:'pawer2.in' });
+            gsap.killTweensOf(arrowLine);
+            gsap.to(arrowLine, { 
+                attr: { d: 'M45 6H0' }, 
+                duration: 1, 
+                ease: 'pawer2.in',
+                onComplete: () => {
+                    arrowLine.setAttribute('d', 'M45 6H0');
+                }
+            });
         }
     });
 });
@@ -26,7 +39,7 @@ const Button = () => {
   }, []);
   
   return (
-    <a className="w-full md:w-80 py-5 px-8 flex items-center justify-between rounded-full border border-black mt-8 lg:mt-16 info link arrow-btn xl:hover:px-9 transition-[padding] hover:duration-500" href="#">
+    <a className="w-full md:w-80 py-5 px-8 flex items-center justify-between rounded-full border border-black mt-8 lg:mt-16 info link arrow-btn xl:hover:px-10 duration-500 lg:hover:w-[22rem] ease-out" href="#">
         <p className="text-base">Learn more</p>
         <span className="block">
         <svg width="50" height="12" viewBox="0 0 67 12" fill="none" xmlns="http://www.w3.org/2000/svg">
